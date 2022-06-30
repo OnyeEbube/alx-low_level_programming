83fb9b0e91cf739c90b909102629be31ceafe868
OnyeEbube <okontaonyeka@gmail.com>
2022-06-30 03:08:40 -0700

    Adding 4-puts.c file againnn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,9 @@
-#include<stdio.h>                                                                                                                     
-
-/**                                                                                                                                   
-
-*main is the ebtry point                                                                                                              
-
-*                                                                                                                                     
-
-*Return value is set to 0                                                                                                             
-
-*/                                                                                                                                    
-
-int main(void)                                                                                                                        
-
-{                                                                                                                                     
-
-        puts("\"Programming is like building a multilingual puzzle");
-
+#include<stdio.h>
+/**
+* main defines the input
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
