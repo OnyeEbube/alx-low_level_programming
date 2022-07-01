0bd1c18e8805d91c1cd76dbe69d2b9cbf1fd1cea
OnyeEbube <okontaonyeka@gmail.com>
2022-07-01 09:44:57 -0700

    Adding 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	char alpha = a;
+
+	for (; alpha <= 122; alpha++)
+	{
+		putchar(alpha\n);
+	}
+	return (0);
+}
