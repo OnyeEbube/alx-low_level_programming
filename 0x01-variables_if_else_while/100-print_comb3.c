edc764f08fbc7d494bd1efa2e5127d14e6aad62c
OnyeEbube <okontaonyeka@gmail.com>
2022-07-01 16:12:20 -0700

    Adding 100-print_comb3.c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,17 @@ int main(void)
 	for (a = 48; a < 58 ; a++)
 	{
 
-		for (b = 49; b < 58; b++)
+		for (b = 48; b < 58; b++)
 		{
 
 			if (a != b && a < b)
 			{
 				putchar(a);
 				putchar(b);
+				if (a == 56 && b == 57)
+				{
+					break;
+				}
 				putchar(',');
 				putchar(' ');
 			}
