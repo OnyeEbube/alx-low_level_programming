6a8f1efd12754c2553c245a25bd84c7f4076d44b
OnyeEbube <okontaonyeka@gmail.com>
2022-07-01 15:53:37 -0700

    Adding 100-print_comb3.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main -Prints combination of two digits
+ *
+ * Return: always returns 0 for success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a < 58 ; a++)
+	{
+
+		for (b = 49; b < 58; b++)
+		{
+
+			if (a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
