807995cabf561be7368ae73cf7221131f4753930
OnyeEbube <okontaonyeka@gmail.com>
2022-07-05 13:57:58 -0700

    Adding 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,15 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			_putchar(c + '0');
+			if (c <= 9)
+			{
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
 			if (b == 9)
 				break;
 			_putchar(',');
