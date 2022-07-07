3d5a5f881c5cfb64a4ce58fe559bde1d706a4493
OnyeEbube <okontaonyeka@gmail.com>
2022-07-07 16:10:54 -0700

    Adding 101-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,28 @@
 
 void print_number(int n)
 {
-	unsigned int m = n;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		m = -m;
+		m = n * -1;
 	}
-	if (m / 10)
+	else
 	{
-		_putchar(m / 10);
+		m = n;
 	}
-	_putchar(m % 10 + '0');
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
