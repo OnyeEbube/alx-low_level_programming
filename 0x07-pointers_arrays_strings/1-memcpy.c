f9a979caec28a29569ef8b7ead532dfec56a3cef
OnyeEbube <okontaonyeka@gmail.com>
2022-07-18 01:16:07 -0700

    Adding 2 files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from a memory area src to a memory area dest
+ * @dest: The destination memory
+ * @src: The source memory
+ * @n: the bytes to be copied
+ *
+ * Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	for ( ; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
