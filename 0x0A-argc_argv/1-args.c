88ea2684ca02bfc29a0e87f3095082caf5795ef6
OnyeEbube <okontaonyeka@gmail.com>
2022-07-22 14:34:37 -0700

    Adding 1-args.c file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
- * main -  prints the number of arguments passed into it.
- * @argc: the argument count
- * @argv: the argument vector
- *
- * Return: 0 - Success
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(argc, *argv[]__attribute__((unused)))
+
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
