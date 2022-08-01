a068c49e5893010d7a73cbffee9ce3b9c69547b2
OnyeEbube <okontaonyeka@gmail.com>
2022-08-01 13:48:44 -0700

    Adding them

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: the new dog's name
+ * @age: its age
+ * @owner: its owner
+ *
+ * Return: struct dog and null if it fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+	int i, namlen, ownlen;
+
+	ptr = malloc(sizeof(*ptr));
+
+	if (ptr == NULL || !(name) || !(owner))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (namlen = 0; name[namlen] != '\0'; namlen++)
+		;
+	for (ownlen = 0; owner[ownlen] != '\0'; ownlen++)
+		;
+	ptr->name = malloc(namlen + 1);
+	ptr->owner = malloc(ownlen + 1);
+
+	if (!(ptr->name) || !(ptr->owner))
+	{
+		free(ptr->name);
+		free(ptr->owner);
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < namlen; i++)
+		ptr->name[i] = name[i];
+	ptr->name = '\0';
+	ptr->age = age;
+	for(i = 0; i < ownlen; i++)
+		ptr->owner[i] = owner[i];
+	ptr->owner = '\0';
+
+	return (ptr);
+}
