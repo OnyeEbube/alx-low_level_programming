29524c81c5974a940da205dac6587e5beb17950f
OnyeEbube <okontaonyeka@gmail.com>
2022-08-11 14:41:13 -0700

    Adding a function that executes before main()

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,7 @@
  * Return: no return
  */
 
-void bmain()__attribute__((constructor));
-void bmain()
+void __attribute__((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
