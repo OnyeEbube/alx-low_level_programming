359dd6d676035518d1a167bdffd190bc352f8d04
OnyeEbube <okontaonyeka@gmail.com>
2022-08-11 14:10:39 -0700

    Updating 4-free_list.c file

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,11 +9,10 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	temp = head;
-	while (temp != NULL)
+	while ((temp = head) != NULL)
 	{
 		head = head->next;
 		free(temp->str);
 		free(temp);
-	}	
+	}
 }
