5acbc75e68044697011d2a098baa01764fcba97e
OnyeEbube <okontaonyeka@gmail.com>
2022-08-11 04:32:11 -0700

    Adding 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ *
+ * Return: returns the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t x;
+
+	x = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
