b28ecee0a423993d7b52335482e39560f50e77b9
OnyeEbube <okontaonyeka@gmail.com>
2022-08-16 08:36:54 -0700

    Adding 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,17 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *temp1;
 
-	while ((temp = *head) != NULL)
+	if (head != NULL)
 	{
-		free(temp);
+		temp1 = *head;
+
+		while ((temp = temp1) != NULL)
+		{
+			temp1 = temp1->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
