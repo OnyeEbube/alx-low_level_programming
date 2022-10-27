538a4a65ea4a6b7a391fd53b4bbf982ddccf8591
OnyeEbube <okontaonyeka@gmail.com>
2022-10-27 21:00:34 +0100

    adding 0-hash_table_create.c file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *))
+	table->array = calloc(size, sizeof(hash_node_t *));
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 	return (table);
