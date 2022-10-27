51ad40ea064ed78a69e429059db4791c71db26f3
OnyeEbube <okontaonyeka@gmail.com>
2022-10-27 21:44:57 +0100

    Adding 2-key_index.c file

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the key index of a key
+ * @key: the key of the data value
+ * @size: the size of the table
+ *
+ * Return: the index of the key/value pair
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	if (key == NULL || size == 0)
+		return (0);
+	index = (hash-djb2(key) % size);
+	return (index);
+}
