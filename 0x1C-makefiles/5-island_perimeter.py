8f2c19db767709214b9ffe63108d6544cadd38b5
OnyeEbube <okontaonyeka@gmail.com>
2022-12-07 19:57:23 +0000

    Adding 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -28,5 +28,5 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
-                perimeter += num_water_neighbours(grid, i, j)
+                perimeter += num_water_neighbors(grid, i, j)
     return perimeter
