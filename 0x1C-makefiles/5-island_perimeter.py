4f882de3eb573636e8a14f3198a8d60761dfafe7
OnyeEbube <okontaonyeka@gmail.com>
2022-12-07 19:51:02 +0000

    Adding 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A function module"""
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
+
+
+def island_perimeter(grid):
+    """ This function returns the perimeter of the island described in grid 
+    Args:
+        grid - is a list of integers
+    """
+    perimeter = 0
+    for in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += num_water_neighbours(grid, i, j)
+    return perimeter
